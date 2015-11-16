982b533c3ff09b051fdffdf9dd9673ac9cf1bdec
elyth91 <elyth91@gmail.com>
2015-11-16 15:08:51 +0100

    gdfgdfgd

---
+++ CGD interface/CGD interface/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CGD_interface
+{
+    [Serializable]
+    public class player
+    {
+        public string l_name;
+        public string passw;
+    }
+
+    public interface Iserver
+    {
+        void login(string name, string pass);
+        List<player> get_logined();
+    }
+}
