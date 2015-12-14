142b4b2f8053ebd4068dd800822f2517bb4994c9
almadiniki <almadi.niki@gmail.com>
2015-12-14 17:49:16 +0100

    otthoni munka 17:48

---
+++ CGD interface/CGD interface/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CGD_interface
+{
+    public interface IPeldanyosit
+    {
+        string peldanytkeszit();
+    }
+    public interface Iserver
+    {
+        bool login(string name, string pass, string id);
+        void logout();
+        List<string> get_players();
+        string get_id(string name);
+        void set_klient(string name);
+        string offer_game(string name);
+    }
+    public interface Ikliens
+    {
+        void refresh_lobby();
+        bool game_require(string name, string id);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+WPF CardGameDuels -  RFT
+
+Az olvass el file segít a felhasználónak rájönni, hogy mirõl is szól ez az egész! :)
+
+Ez egy online kártyajáték lesz, ahol önfeledten szórakozhatsz órák hosszat magadban vagy akár
+más játékosokkal szemben is.
