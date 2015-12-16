1428b2ad9d03676887f097d04557e019e2a0289b
almadiniki <almadi.niki@gmail.com>
2015-12-16 13:59:20 +0100

    lobby gamefield és a fő ablak desingeénak módosítása, gombok szerkesztése, listbox hozzáadása, és maga a kinézet létrehozása (2h)

---
+++ CGD kliens/CGD kliens/gamefield.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CGD_interface;
+
+namespace CGD_kliens
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Iserver b;
+        string id;
+        public MainWindow()
+        {
+            InitializeComponent();
+            b = null;
+            connect();
+        }
+
+        void connect()
+        {
+            try
+            {
+                IPeldanyosit m = (IPeldanyosit)Activator.GetObject(typeof(IPeldanyosit), "tcp://localhost:8085/SzerverPeldanyosito");
+                id = m.peldanytkeszit();
+                b = (Iserver)Activator.GetObject(typeof(Iserver), "tcp://localhost:8085/" + id);
+            }
+            catch { MessageBox.Show("Nincs kapcsolat a szerverrel"); }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult r = MessageBox.Show("Biztos kilépsz?", "Kilépés", MessageBoxButton.YesNo);
+            if (r == MessageBoxResult.Yes)
+            {
+                if (b != null)
+                {
+                    b.logout();
+                }
+                this.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (b.login(l_name.Text, passwd.Password,id))
+                {
+                    MessageBox.Show("bejelentkezés sikeres!");
+                    Lobby lw = new Lobby(b, id);
+                    this.Visibility = Visibility.Collapsed;
+                    lw.ShowDialog();
+                    this.Visibility = Visibility.Visible;
+                    b.logout();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("sikertelen bejelentkezés!");
+                }
+
+            }
+            catch { MessageBox.Show("Hiba"); }
+        }
+    }
+}
